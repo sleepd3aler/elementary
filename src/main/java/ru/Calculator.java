46bed67a7967d65229391f0f043fa6b8020f32be
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-18 11:29:38 +0300

    Задание 1.3 Арифметические операции

---
+++ src/main/java/ru/Calculator.java
@@ -0,0 +1,9 @@
+package ru;
+
+public class Calculator {
+    public static void main(String[] args) {
+    int six = 6, four = 4, five = 5;
+    int sixDivTwo = six / 2, fiveMinusTwo = five - 2, fourTimesTwo = four * 2;
+        System.out.println(sixDivTwo + "\n" + fiveMinusTwo + "\n" + fourTimesTwo);
+    }
+}
