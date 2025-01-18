ffcf99ac37617a706351b6e84b6de3b60a732eb9
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-18 11:21:23 +0300

    Задание 1.1 Определение типа переменных

---
+++ src/main/java/ru/Peremennie.java
@@ -0,0 +1,14 @@
+package ru;
+
+public class Peremennie {
+    public static void main(String[] args) {
+        short age = 31;
+        char gender = 'M';
+        float height = 1.86f;
+        short bricks = 99;
+        int koe = 2000000;
+        long spacedistance = 12345678990L;
+        boolean work = true;
+        byte info = 32;
+    }
+}
