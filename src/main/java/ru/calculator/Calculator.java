d52d704ed5f120e393bd65638f276918243620a8
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-18 12:25:21 +0300

    Исправил вывод в консоль

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -4,6 +4,8 @@ public class Calculator {
     public static void main(String[] args) {
     int six = 6, four = 4, five = 5;
     int sixDivTwo = six / 2, fiveMinusTwo = five - 2, fourTimesTwo = four * 2;
-        System.out.println(sixDivTwo + "\n" + fiveMinusTwo + "\n" + fourTimesTwo);
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimesTwo);
     }
 }
