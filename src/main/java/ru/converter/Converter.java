313ea4c8e3e4ef53645722c8158131bf133e8ef2
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-18 18:07:18 +0300

    Задание 2 Тестирование

--- src/main/java/ru/converter/Converter.java
+++ src/main/java/ru/converter/Converter.java
@@ -14,9 +14,13 @@ public class Converter {
     public static void main(String[] args) {
         float in = 140;
         float expected = 1.4f;
+        float expectedInDollars = 1.5555556f;
         float out = Converter.rubleToEuro(in);
+        float outInDollars = rubleToDollar(in);
         boolean passed = expected == out;
+        boolean passedInDollars = expectedInDollars == outInDollars;
         System.out.println("140 rubles are 1.4. Test result : " + passed);
+        System.out.println("140 rubles are 1.5555556. Test result : " + passedInDollars);
     }
 }
 
