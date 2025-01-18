4ef13e6df5a40f112dd38494360cc63b14e03973
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-18 22:25:34 +0300

    Задание 2.2 Что такое import

--- src/main/java/ru/calculator/MathCalculator.java
+++ src/main/java/ru/calculator/MathCalculator.java
@@ -24,6 +24,5 @@ public class MathCalculator {
         System.out.println("Результат расчета суммы умножения равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета суммы деления и вычитания равен: " + sumAndDivide(10, 20));
         System.out.println("Результат расчета суммы вычисления всех операций равен: " + sumOfAll(10, 20));
-
     }
 }
