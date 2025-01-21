229cbe9d21d604353d1d0e4d9453884df2e839e8
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-21 23:22:39 +0300

    Задание 3.16 Отладка IDEA

--- src/main/java/ru/condition/ThreeMax.java
+++ src/main/java/ru/condition/ThreeMax.java
@@ -2,12 +2,19 @@ package ru.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = first > second ? first : second > third ? second : third;
+        int result = first;
+        if (second > first) {
+            result = second;
+        }
+        if (third > result) {
+            result = third;
+        }
+
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(max(1, 5, 3));
+        System.out.println(max(1, 2, 3));
     }
 }
 
