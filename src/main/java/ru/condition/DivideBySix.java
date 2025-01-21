6d22a3f23d351a1dd9baf22177d6c1d4a554c10f
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-21 22:31:40 +0300

    Задание 3.15 Вложенный цикл if / else

--- src/main/java/ru/condition/DivideBySix.java
+++ src/main/java/ru/condition/DivideBySix.java
@@ -3,11 +3,11 @@ package ru.condition;
 public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
-        if (number % 3 == 0 && number % 2 == 0) {
+        if (number % 6 == 0) {
             rsl = "The number divides by 6.";
-        } else if (number % 3 == 0 && number % 2 != 0) {
+        } else if (number % 3 == 0) {
             rsl = "The number divides by 3, but it isn't the even number.";
-        } else if (number % 3 != 0 && number % 2 == 0) {
+        } else if (number % 2 == 0) {
             rsl = "The number doesn't divide by 3, but it is the even number.";
         } else {
             rsl = "The number doesn't divide by 3 and it isn't the even number.";
@@ -16,7 +16,7 @@ public class DivideBySix {
     }
 
     public static void main(String[] args) {
-        System.out.println(checkNumber(24));
+        System.out.println(checkNumber(14));
     }
 }
 
