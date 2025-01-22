9c9747b270aa5cda6c8c2246d045fe512f330a94
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-22 22:11:13 +0300

    Задание 4.8 Простое число, оператор прерывания break

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -2,7 +2,7 @@ package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number <= 1 ? true : false;
+        boolean prime = number > 1 ? true : false;
 
         for (int i = 2; i < number; i++) {
             if (number % 2 == 0) {
