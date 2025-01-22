e3ba45905fe109a71bb06053a0da3291470f653d
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-22 16:33:23 +0300

    Задание 4.7 Протеиновая диета

---
+++ src/main/java/ru/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nikolay) {
+        int month = 0;
+        while (ivan <= nikolay) {
+            ivan *= 3;
+            nikolay *= 2;
+            month++;
+        }
+        return month;
+    }
+}
