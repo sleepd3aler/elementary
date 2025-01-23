0ee25231a4b1bf913dff7cc5ea806c4143279592
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-23 17:27:16 +0300

    Задание 4.8 Простое число, оператор break;

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -4,7 +4,7 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = number >= 2;
         for (int i = 2; i < number; i++) {
-            if (number % 2 == 0) {
+            if (number % i == 0) {
                 prime = false;
                 break;
             }
@@ -14,7 +14,7 @@ public class CheckPrimeNumber {
     }
 
     public static void main(String[] args) {
-        System.out.println(check(5));
+        System.out.println(check(3));
     }
 }
 
