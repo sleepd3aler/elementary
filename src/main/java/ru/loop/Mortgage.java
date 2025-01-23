498e29331c02257df19ea13826a2b2a8759f2f08
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-23 20:02:35 +0300

    Задание 4.10 Ипотека

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -1,20 +1,17 @@
 package ru.loop;
 
 public class Mortgage {
-    public static int year(double amount, int selary, double percent) {
+    public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            if (percent < 1) {
-                percent *= 100;
-            }
             amount += amount * percent / 100;
-            amount -= selary;
+            amount -= salary;
             year++;
         }
         return year;
     }
 
     public static void main(String[] args) {
-        System.out.println(year(100, 120, 0.5));
+        System.out.println(year(100, 120, 50));
     }
 }
