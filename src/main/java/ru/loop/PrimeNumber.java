bb60115f76765c4212b38f3866e04d15e4501a29
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-23 14:55:31 +0300

    Задание 4.9 Простые числа

---
+++ src/main/java/ru/loop/PrimeNumber.java
@@ -0,0 +1,24 @@
+package ru.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(11));
+    }
+}
