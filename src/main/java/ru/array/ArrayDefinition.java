930a13e0b23da2d5910f736d641a9e2324aba20c
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-24 20:37:01 +0300

    Задание 5.3 Заполнение Массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -2,11 +2,15 @@ package ru.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println(ages.length);
-        System.out.println(surnames.length);
-        System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Artem";
+        names[1] = "Alex";
+        names[2] = "Bob";
+        names[3] = "Charlie";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
