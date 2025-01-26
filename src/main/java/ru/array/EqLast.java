66a234e9f39979421e8e1aa351b389bb3743befc
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-27 00:32:41 +0300

    Задание 5.15 Сравнить последние элементы двух массивов

--- src/main/java/ru/array/EqLast.java
+++ src/main/java/ru/array/EqLast.java
@@ -2,7 +2,6 @@ package ru.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        boolean result = left[left.length - 1] == right[right.length - 1];
-        return result;
+        return left[left.length - 1] == right[right.length - 1];
     }
 }
