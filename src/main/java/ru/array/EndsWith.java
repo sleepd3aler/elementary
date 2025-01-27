93b7c4f2bde5223bd49567ad47e1163bad826366
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-27 22:10:41 +0300

    Задание 5.17 Слово заканчивается на...

--- src/main/java/ru/array/EndsWith.java
+++ src/main/java/ru/array/EndsWith.java
@@ -2,12 +2,11 @@ package ru.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = true;
         for (int i = 0; i < post.length; i++) {
             if (word[word.length - i - 1] != post[post.length - i - 1]) {
-                result = false;
+                return false;
             }
         }
-        return result;
+        return true;
     }
 }
