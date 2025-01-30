9f83de4c4405d8986f5ad724c6aee0f2ce399d82
sleepd3aler <enjoydeathcore228@gmail.com>
2025-01-30 16:01:42 +0300

    5.31 Метод двух указателей

--- src/main/java/ru/array/TwoNumberSum.java
+++ src/main/java/ru/array/TwoNumberSum.java
@@ -2,13 +2,19 @@ package ru.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-        SortSelected.sort(array);
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == target) {
-                    return new int[]{i, j};
-                }
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int sum = array[left] + array[right];
+            if (sum == target) {
+                return new int[]{left, right};
             }
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+
         }
         return new int[0];
     }
