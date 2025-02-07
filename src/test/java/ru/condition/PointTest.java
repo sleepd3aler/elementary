0fffec8239189727dbf7e637f5e432d4846e9093
sleepd3aler <enjoydeathcore228@gmail.com>
2025-02-07 19:40:16 +0300

    7.13 Рефакторинг Расстояние между точками

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -4,51 +4,42 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withPrecision;
-import static ru.condition.Point.distance;
 
 public class PointTest {
 
     @Test
     void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double result = a.distance(b);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when00toMinus50then5() {
+        Point a = new Point(0, 0);
+        Point b = new Point(-5, 0);
+        double result = a.distance(b);
         double expected = 5;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = -5;
-        int y2 = 0;
-        double out = distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when22to22then0() {
+        Point a = new Point(2, 2);
+        Point b = new Point(2, 2);
+        double result = a.distance(b);
         double expected = 0;
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 2;
-        double out = distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when10to00Then1() {
+        Point a = new Point(1, 0);
+        Point b = new Point(0, 0);
+        double result = a.distance(b);
         double expected = 1;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
-        double out = distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 }
